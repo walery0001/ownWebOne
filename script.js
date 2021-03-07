8d705e726b22da239063f5370be0caa415958508
walery0001 <marcin.walery@gmail.com>
2021-03-07 18:59:41 +0100

    second page

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let img = document.getElementById('eyesImg');
 let container = document.getElementById('container');
 let divLinkImg = document.querySelectorAll('.divLinkImg');
+let tekstLink = document.querySelector('.tekstLink');
 
-container.addEventListener('mousemove',()=>{
+container.addEventListener('mousemove',moveBigPic,{once:true})
+   function moveBigPic(){
     img.classList.add('oneEyes');
    
-    divLinkImg.forEach(e=>{e.classList.add('keyFrames')});
-    divLinkImg.forEach(e=> e.style.visibility = "visible");
+    divLinkImg.forEach(e=>e.classList.add('keyFrames'));
+    divLinkImg.forEach(e=> e.style.visibility = 'visible');
     
-})
+}
 
+divLinkImg.forEach(e=> e.addEventListener('click',clickHome))
 
+function clickHome(){
+	divLinkImg.forEach(e=> e.style.visibility = 'hidden');
+	  tekstLink.style.display = 'block';
+}
