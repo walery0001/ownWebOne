f0fd7d8cbd802997df9796b886e6f6c729c554c5
walery0001 <marcin.walery@gmail.com>
2021-04-21 19:32:59 +0200

    handy laptop ok

--- script.js
+++ script.js
@@ -10,18 +10,19 @@ const linkSources = [
     id: 2,
     topic: "Omnie",
     source:
-      " miały pomóc jego naśladowcom sprawiedliwie uczył, że została oddana sprawiedliwość Jego wybranym, którzy wołają do Niego . W gruncie rzeczy słowa te są obietnicą: Nasz Bóg jest świadomy, przez co przechodzimy w dniach ostatnich, i w odpowiednim czasie uczyni zadość sprawiedliwości (2 Tes. 1:6). Kiedy stosujemy zasady, których uczył Jezus, traktujemy innych sprawiedliwie. A jeśli padliśmy ofiarą niesprawiedliwości panującej w świecie Szatana, pociesza nas świadomość, że Jehowa na pewno się tym zajmie. w19.05 s. 5, ak. 18, 19 ",
+      "miały pomóc jego naśladowcom spremy innych sprawiedliwie. ",
   },
   {
     id: 3,
     topic: "portfolio",
-    source: " sprawiedliwość Jego wybranym, którzy wołają do Niego",
+    source: `<img class="clickImg" src="grafik/hubert/h00.jpeg"/>`,
   },
   {
     id: 4,
-    topic: "kontakt",
-    source: " sprawiedliwość Jego wybranym, którzy wołają do Niego",
+    topic: "Kontakt",
+    source: `Kamienica 62-530 ${'\n'}telefon:345 432 908 ${'\n'}mail:hubert@gmail.com`,
   },
+ 
 ];
 
 let img = document.getElementById("eyesImg");
@@ -31,6 +32,8 @@ let tekstLink = document.querySelector(".tekstLink");
 let topicText = document.querySelector(".topicText");
 let topicSourceLink = document.querySelectorAll(".topicSourceLink");
 let navLink = document.querySelectorAll(".navLink");
+let imgBlackBg = document.querySelector(".imgBlackBg");
+
 //click and move big picture
 
 container.addEventListener("mousemove", moveBigPic, { once: true });
@@ -43,7 +46,7 @@ function moveBigPic() {
   });
   setTimeout(() => {
     divLinkImg.forEach((e) => e.addEventListener("click", clickHome));
-  }, 1000);
+  }, 2000);
 }
 
 //click navLink and change tekst topic
@@ -51,7 +54,8 @@ function moveBigPic() {
 
 //show Topic_menu Text topic_Links
 function clickHome(f) {
-const targetLink =	f.target.textContent
+const targetLink =	f.target.className
+console.log(f.target.classList)
   divLinkImg.forEach((e) => {
 	  
 	  e.style.visibility = "hidden";
@@ -61,46 +65,79 @@ const targetLink =	f.target.textContent
   topicSourceLink.forEach((e) => {
     e.style.display = "block";
 	e.addEventListener('click' ,el=>{
-		const targetel =	el.target.textContent
+		let targetel = el.target.textContent
+	
+if(el.target.src === `file:///G:/java%20script%20lerning/own_Web/grafik/hubert/h00.jpeg`){
+	 el.target.src = `file:///G:/java%20script%20lerning/own_Web/grafik/hubert/h1.jpeg`
+	for(i=1;i<18;i++){	
+	tekstLink.innerHTML += `<img class="firstFotoGalery" src="grafik/hubert/h${i}.jpeg"/>`}
+}
+
+//show small pic BIG
+if(el.target.classList[0] === 'firstFotoGalery'){
+	imgBlackBg.style.display = "block"
+	
+	let imgForDiv = el.target.src.split('file:///G:/java%20script%20lerning/own_Web/')
+
+	imgBlackBg.innerHTML =`<img class="fullImg" src="${imgForDiv[1]}"/>`
+
+}
+//close big pic
+imgBlackBg.addEventListener('click',()=>
+	imgBlackBg.style.display = "none")
+
+
+	//link Home back to HOME
+	
 		if(targetel === 'Home'){
 			divLinkImg.forEach((e) => {
 				e.classList.add("keyFrames");
 				e.style.visibility = "visible";
 				topicSourceLink.forEach((e) => {
 					e.style.display = "none"})
+
 			  });
 
 		}
+	//	text links / links click
 		if(targetel === 'Omnie'){
 			topicText.innerText = linkSources[1].topic;
 			tekstLink.innerText = linkSources[1].source;
 		}
 		if(targetel === 'Portfolio'){
 			topicText.innerText = linkSources[2].topic;
-			tekstLink.innerText = linkSources[2].source;
+			tekstLink.innerHTML = linkSources[2].source;
+
 		}
+
 		if(targetel === 'Kontakt'){
 			topicText.innerText = linkSources[3].topic;
 			tekstLink.innerText = linkSources[3].source;
 		}
 	})
   });
-
-if(targetLink === 'Home'){
+//pictures links / links click
+if(targetLink === 'home'){
 	topicText.innerText = linkSources[0].topic;
 	tekstLink.innerText = linkSources[0].source;
 }
-if(targetLink === 'Omnie'){
+if(targetLink === 'aboutMe' || 'Omnie'){
 	topicText.innerText = linkSources[1].topic;
 	tekstLink.innerText = linkSources[1].source;
 }
-if(targetLink === 'Portfolio'){
+if(targetLink === 'portfolio' || 'Portfolio'){
 	topicText.innerText = linkSources[2].topic;
-	tekstLink.innerText = linkSources[2].source;
+	tekstLink.innerHTML = linkSources[2].source;
+
 }
-if(targetLink === 'Kontakt'){
+if(targetLink === 'kontaktImg' || 'Kontakt'){
 	topicText.innerText = linkSources[3].topic;
 	tekstLink.innerText = linkSources[3].source;
 }
 
 }
+//link	to foto galery
+
+const widthWindow = window.screen.width;
+widthWindow < 430 ?
+	eyesImg.src = 'grafik/jankaa1.jpg':true
